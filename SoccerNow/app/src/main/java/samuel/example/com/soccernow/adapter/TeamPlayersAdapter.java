b725ae2bfc0f674f4fc6457e7e0fb8004151b1ee
samuel <samuelsamer952@gmail.com>
2017-07-15 12:30:56 +0200

    adding player item design

---
+++ SoccerNow/app/src/main/java/samuel/example/com/soccernow/adapter/TeamPlayersAdapter.java
@@ -0,0 +1,74 @@
+package samuel.example.com.soccernow.adapter;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+import samuel.example.com.soccernow.R;
+import samuel.example.com.soccernow.model.football.PlayerData;
+import samuel.example.com.soccernow.model.football.leagueMatches.MatcheData;
+
+/**
+ * Created by samuel on 7/15/2017.
+ */
+
+public class TeamPlayersAdapter   extends RecyclerView.Adapter<TeamPlayersAdapter.RecyclerViewAdapterHolder>{
+
+    private List<PlayerData> playerDataList;
+    public void setApiResponse ( List<PlayerData> playerDataList )
+    {
+        this.playerDataList =playerDataList;
+        notifyDataSetChanged();
+    }
+
+
+
+    @Override
+    public TeamPlayersAdapter.RecyclerViewAdapterHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        Context context = parent.getContext() ;
+        int layoutPhotoItem = R.layout.player_item;
+        LayoutInflater inflater = LayoutInflater.from(context);
+        boolean shouldAttachToParentImmediately = false;
+        View view = inflater.inflate(layoutPhotoItem ,parent ,shouldAttachToParentImmediately);
+        return new TeamPlayersAdapter.RecyclerViewAdapterHolder(view);    }
+
+    @Override
+    public void onBindViewHolder(TeamPlayersAdapter.RecyclerViewAdapterHolder holder, final int position) {
+        PlayerData playerData = playerDataList.get(position);
+        holder.playerNum.setText(String.valueOf(playerData.getJerseyNumber()));
+        holder.playername.setText(playerData.getName());
+        holder.playerPosition.setText(playerData.getPosition());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        if(playerDataList!=null)
+        {
+            return playerDataList.size();
+        }
+        return 0;
+    }
+
+
+    public class RecyclerViewAdapterHolder extends RecyclerView.ViewHolder {
+        private TextView playerNum ;
+        private TextView playername ;
+        private TextView playerPosition ;
+
+        public RecyclerViewAdapterHolder(View itemView) {
+            super(itemView);
+            playerNum= (TextView) itemView.findViewById(R.id.player_num);
+            playername= (TextView) itemView.findViewById(R.id.play_name);
+            playerPosition = (TextView) itemView.findViewById(R.id.player_position);
+        }
+
+    }
+
+}
--- SoccerNow/app/src/main/java/samuel/example/com/soccernow/view/football/TeamPlayersActivity.java
+++ SoccerNow/app/src/main/java/samuel/example/com/soccernow/view/football/TeamPlayersActivity.java
@@ -3,6 +3,9 @@ package samuel.example.com.soccernow.view.football;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.List;
@@ -11,6 +14,7 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 import samuel.example.com.soccernow.R;
+import samuel.example.com.soccernow.adapter.TeamPlayersAdapter;
 import samuel.example.com.soccernow.model.ApiInterface;
 import samuel.example.com.soccernow.model.football.PlayerData;
 import samuel.example.com.soccernow.model.football.PlayerResponse;
@@ -24,17 +28,31 @@ import static samuel.example.com.soccernow.view.football.LeagueTableFragment.TEA
 public class TeamPlayersActivity extends AppCompatActivity {
     private int teamId;
     private List<PlayerData> playerDataList;
+    private TeamPlayersAdapter teamPlayersAdapter ;
+    private RecyclerView mRecyclerView;
+    private TextView teamName;
+    private ImageView teamImage;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_team_players);
+        mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        teamPlayersAdapter = new TeamPlayersAdapter();
         if (getIntent()!=null && getIntent().getBundleExtra(TEAM_BUNDEL)!=null)
         {
             Bundle bundle = getIntent().getBundleExtra(TEAM_BUNDEL);
             if(bundle.getString(TEAM_CODE)!=null && bundle.getString(TEAM_NAME)!=null && bundle.getString(TEAM_IMAGE)!=null )
             {
+                setTitle(getResources().getString(R.string.teamsplayers));
                 teamId = Integer.valueOf(bundle.getString(TEAM_CODE));
+                String name = bundle.getString(TEAM_NAME);
+                String teamUri = bundle.getString(TEAM_IMAGE);
+                teamName = (TextView) findViewById(R.id.teamTextView);
+                teamImage = (ImageView) findViewById(R.id.teamImageView) ;
+                teamName.setText(name);
+                teamImage.setImageDrawable(getResources().getDrawable(R.drawable.p436));
                 loadLeagueTable ();
             }
         }
@@ -44,9 +62,9 @@ public class TeamPlayersActivity extends AppCompatActivity {
 
     private void loadLeagueTable ()
     {
-        /*
-        mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
-        mRecyclerView.setAdapter(leagueTableAdapter);*/
+
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(TeamPlayersActivity.this));
+        mRecyclerView.setAdapter(teamPlayersAdapter);
 
         ApiInterface apiService = ApiInterface.ApiClientFootBall.getClient().create(ApiInterface.class);
         Call<PlayerResponse> call = apiService.getTeamPlayers(teamId);
@@ -54,7 +72,7 @@ public class TeamPlayersActivity extends AppCompatActivity {
             @Override
             public void onResponse(Call<PlayerResponse> call, Response<PlayerResponse> response) {
                 playerDataList = response.body().getPlayerDataList();
-                int si =playerDataList.size();
+                teamPlayersAdapter.setApiResponse(playerDataList);
             }
 
             @Override
